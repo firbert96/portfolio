75e174a6c292be1a8d1659f6ac78463b460d7ef0
firbert96 <firbert96@gmail.com>
2020-05-18 00:22:22 +0700

    [FIRB] fixing bug in certification

--- build/js/script.js
+++ build/js/script.js
@@ -44,3 +44,28 @@ $(window).scroll(function(){
 
 	}
 });
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementsByClassName("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+$(document).ready(function(){
+	$(".myImg").click(function(event){
+	  event.preventDefault();
+	  modal.style.display = "block";
+  	  modalImg.src = this.src;
+      captionText.innerHTML = this.alt;
+
+	});
+  });
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
